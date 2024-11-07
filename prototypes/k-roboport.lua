2577db75a9ecb4061768aeba8156b1495792f08a
Alan Baines <abaines.vt+github@gmail.com>
2024-11-06 23:37:05 -0500

    remove dead code and logs

--- prototypes/k-roboport.lua
+++ prototypes/k-roboport.lua
@@ -36,17 +36,9 @@ local recipe = {
 data:extend{recipe}
 
 local function scale(object, scale)
-    -- log("BEFORE "..sb(object))
     for k1, v1 in ipairs(object) do
-        -- logsb(k1)
-        -- logsb(v1)
-        for k2, v2 in ipairs(v1) do
-            -- logsb(k2)
-            -- logsb(v2)
-            object[k1][k2] = v2 * scale
-        end
+        for k2, v2 in ipairs(v1) do object[k1][k2] = v2 * scale end
     end
-    -- log("AFTER "..sb(object))
 end
 
 local k_roboport = table.deepcopy(data.raw["roboport"]["roboport"])
@@ -67,32 +59,11 @@ k_roboport.recharging_light.color = {r = 1.0, g = 0.4, b = 0.0}
 k_roboport.recharging_light.intensity = 1
 k_roboport.recharging_light.size = 7
 
---[[
-log("------------------------------------------------------------")
-logsb(k_roboport.charging_offsets)
-logsb(k_roboport.collision_box)
-logsb(k_roboport.selection_box)
-log("============================================================")
-]] --
-
 local scale_ratio = 1.25
 
 scale(k_roboport.charging_offsets, scale_ratio)
 scale(k_roboport.collision_box, scale_ratio)
 scale(k_roboport.selection_box, scale_ratio)
-
---[[
-logsb(k_roboport.charging_offsets)
-logsb(k_roboport.collision_box)
-logsb(k_roboport.selection_box)
-log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-]] --
-
---[[
-charging_offsets *= 1.25
-collision_box *= 1.25
-selection_box *= 1.25
-]] --
 
 k_roboport.base.layers[1].scale = k_roboport.base.layers[1].scale * scale_ratio
 k_roboport.base.layers[2].scale = k_roboport.base.layers[2].scale * scale_ratio
@@ -128,139 +99,6 @@ k_roboport.recharging_animation.scale = scale_ratio *
 k_roboport.water_reflection.pictures.scale = scale_ratio *
                                                  k_roboport.water_reflection
                                                      .pictures.scale
-
-local entityold = {
-    base = {
-        layers = {
-            {
-                filename = "__base__/graphics/entity/roboport/roboport-base.png",
-                height = 135,
-                scale = 1.25,
-                shift = {0.5, 0.25},
-                width = 143
-            }, {
-                draw_as_shadow = true,
-                filename = "__base__/graphics/entity/roboport/roboport-shadow.png",
-                height = 101,
-                scale = 1.25,
-                shift = {0.890625, 0.6015625},
-                width = 147
-            }
-        }
-    },
-    base_animation = {
-        animation_speed = 0.5,
-        filename = "__base__/graphics/entity/roboport/roboport-base-animation.png",
-        frame_count = 8,
-        height = 31,
-        priority = "medium",
-        scale = 1.25,
-        shift = {-0.53149999999999995 * 1.25 - 0.15, -1.9375 * 1.25},
-        width = 42
-    },
-    base_patch = {
-        filename = "__base__/graphics/entity/roboport/roboport-base-patch.png",
-        frame_count = 1,
-        height = 50,
-        priority = "medium",
-        scale = 1.25,
-        shift = {0.03125 - 0.1, 0.203125 - 0.0},
-        width = 69
-    },
-    charge_approach_distance = 5,
-    charging_energy = "500kW",
-    charging_offsets = {
-        {-1.5 * 1.25, -0.5 * 1.25 - 0.4}, {1.5 * 1.25, -0.5 * 1.25 - 0.4},
-        {1.5 * 1.25, 1.5 * 1.25 - 0.4}, {-1.5 * 1.25, 1.5 * 1.25 - 0.4}
-    },
-    close_door_trigger_effect = {
-        {
-            sound = {
-                filename = "__base__/sound/roboport-door.ogg",
-                volume = 0.75
-            },
-            type = "play-sound"
-        }
-    },
-    collision_box = {{-1.7 * 1.25, -1.7 * 1.25}, {1.7 * 1.25, 1.7 * 1.25}},
-    construction_radius = 55 / 2.0,
-    corpse = "roboport-remnants",
-    door_animation_down = {
-        filename = "__base__/graphics/entity/roboport/roboport-door-down.png",
-        frame_count = 16,
-        height = 22,
-        priority = "medium",
-        scale = 1.25,
-        shift = {0.015625 * 1.25, -0.234375 * 1.25},
-        width = 52
-    },
-    door_animation_up = {
-        filename = "__base__/graphics/entity/roboport/roboport-door-up.png",
-        frame_count = 16,
-        height = 20,
-        priority = "medium",
-        scale = 1.25,
-        shift = {0.015625 * 1.25, -0.890625 * 1.25},
-        width = 52
-    },
-    draw_construction_radius_visualization = true,
-    draw_logistic_radius_visualization = true,
-    dying_explosion = "medium-explosion",
-    energy_source = {
-        buffer_capacity = "100MJ",
-        input_flow_limit = "5MW",
-        type = "electric",
-        usage_priority = "secondary-input"
-    },
-    energy_usage = "100kW",
-    flags = {"placeable-player", "player-creation"},
-    icon = "__base__/graphics/icons/roboport.png",
-    icon_size = 64,
-    logistics_radius = 25 / 2.0,
-    logistics_connection_distance = 25,
-    material_slots_count = 7 / 2.0,
-    max_health = 500 / 2.0,
-    minable = {mining_time = 0.1, result = "k-roboport"},
-    name = "k-roboport",
-    open_door_trigger_effect = {
-        {
-            sound = {filename = "__base__/sound/roboport-door.ogg", volume = 1},
-            type = "play-sound"
-        }
-    },
-    recharge_minimum = "40MJ",
-    recharging_animation = {
-        animation_speed = 0.5,
-        filename = "__base__/graphics/entity/roboport/roboport-recharging.png",
-        frame_count = 16,
-        height = 35,
-        priority = "high",
-        scale = 1.5,
-        width = 37
-    },
-    recharging_light = {
-        color = {b = 1, g = 1, r = 1},
-        intensity = 0.4,
-        size = 5
-    },
-    request_to_open_door_timeout = 15,
-    resistances = {
-        {percent = 60, type = "fire"}, {percent = 30, type = "impact"}
-    },
-    robot_slots_count = 7 / 2.0,
-    selection_box = {{-2 * 1.25, -2 * 1.25}, {2 * 1.25, 2 * 1.25}},
-    spawn_and_station_height = -0.1,
-    stationing_offset = {0, 0},
-    type = "roboport",
-    working_sound = {
-        audible_distance_modifier = 0.5,
-        max_sounds_per_type = 3,
-        probability = 0.003333333333333333,
-        sound = {filename = "__base__/sound/roboport-working.ogg", volume = 0.6}
-    }
-}
-
-log(sb(k_roboport))
 
 data:extend{k_roboport}
 
